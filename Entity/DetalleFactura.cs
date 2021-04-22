8fb2ea87bdfe46655f6fd011521848a36486f7ae
Anderson0202 <53060401+Anderson0202@users.noreply.github.com>
2021-04-22 15:55:24 -0500

    Agregar archivos de proyecto.

---
+++ Entity/DetalleFactura.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class DetalleFactura
+    {
+        public string IdFactura { get; set; }
+        public Factura Factura { get; set; }
+        public string IdServicio { get; set; }
+        public Servicio Servicio { get; set; }
+        public int Cantidad { get; set; }
+        public string Descripcion { get; set; }
+        public double PrecioUnitario { get; set; }
+        public double Importe { get; set; }
+        public double Total { get; set; }
+
+        public void AgregarDetalleFactura()
+        {
+            Factura factura = new Factura();
+
+        }
+    }
+}
---
+++ Entity/Factura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Factura
+    {
+        public string IdCliente { get; set; }
+        public Persona Persona { get; set; }
+        public long Codigo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public string NumeroServicio { get; set; }
+        public List<DetalleFactura> DetallesFactura { get; set; }
+        public double Total { get; set; }
+
+
+        public void CalcularSubtotal()
+        {
+            double subtotal = 0;
+            foreach (var item in DetallesFactura)
+            {
+                subtotal += item.Total;
+            }
+
+        }
+
+    }
+}
