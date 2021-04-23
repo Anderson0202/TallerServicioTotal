53a0ff137010719c09f2b8f855e2eb888dbe97ab
Anderson0202 <53060401+Anderson0202@users.noreply.github.com>
2021-04-23 00:23:28 -0500

    Base de FrmPrincipal

--- PresentacionGui/Principal.Designer.cs
+++ PresentacionGui/Principal.Designer.cs
@@ -29,13 +29,93 @@ namespace PresentacionGui
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.PnlMenu = new System.Windows.Forms.Panel();
+            this.PnlSuperior = new System.Windows.Forms.Panel();
+            this.PnlPrincipal = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.PnlLogo = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.PnlMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PnlMenu
+            // 
+            this.PnlMenu.BackColor = System.Drawing.Color.Indigo;
+            this.PnlMenu.Controls.Add(this.panel1);
+            this.PnlMenu.Controls.Add(this.button1);
+            this.PnlMenu.Controls.Add(this.PnlLogo);
+            this.PnlMenu.Dock = System.Windows.Forms.DockStyle.Left;
+            this.PnlMenu.Location = new System.Drawing.Point(0, 42);
+            this.PnlMenu.Name = "PnlMenu";
+            this.PnlMenu.Size = new System.Drawing.Size(189, 760);
+            this.PnlMenu.TabIndex = 0;
+            // 
+            // PnlSuperior
+            // 
+            this.PnlSuperior.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlSuperior.Location = new System.Drawing.Point(0, 0);
+            this.PnlSuperior.Name = "PnlSuperior";
+            this.PnlSuperior.Size = new System.Drawing.Size(1156, 42);
+            this.PnlSuperior.TabIndex = 1;
+            // 
+            // PnlPrincipal
+            // 
+            this.PnlPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PnlPrincipal.Location = new System.Drawing.Point(189, 42);
+            this.PnlPrincipal.Name = "PnlPrincipal";
+            this.PnlPrincipal.Size = new System.Drawing.Size(967, 760);
+            this.PnlPrincipal.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button1.Location = new System.Drawing.Point(0, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(189, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // PnlLogo
+            // 
+            this.PnlLogo.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.PnlLogo.Name = "PnlLogo";
+            this.PnlLogo.Size = new System.Drawing.Size(189, 153);
+            this.PnlLogo.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 198);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(189, 99);
+            this.panel1.TabIndex = 2;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1156, 802);
+            this.Controls.Add(this.PnlPrincipal);
+            this.Controls.Add(this.PnlMenu);
+            this.Controls.Add(this.PnlSuperior);
+            this.Name = "Principal";
+            this.Text = "FrmPrincipal";
+            this.PnlMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel PnlMenu;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel PnlLogo;
+        private System.Windows.Forms.Panel PnlSuperior;
+        private System.Windows.Forms.Panel PnlPrincipal;
     }
 }
 
