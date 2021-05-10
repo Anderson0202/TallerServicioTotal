4c4e98c7893843c23da57c2c2ea0240b0ba2252b
Carlos David Villazon Solano <carlosrbm60@gmail.com>
2021-05-09 19:34:51 -0500

    Diagnostico

---
+++ Entity/Diagnostico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Diagnostico
+    {
+        public string Tipo { get; set; }
+        public string Falla { get; set; }
+        public string Escaner { get; set; }
+        
+    }
+}
